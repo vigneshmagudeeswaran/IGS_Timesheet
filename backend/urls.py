4d5bdea19ac5fe415990c0a77d9b193db231f618
Srinivasa Gogul M <srinivasagokul1999@gmail.com>
2023-09-08 19:54:25 +0530

    Update

--- backend/urls.py
+++ backend/urls.py
@@ -3,9 +3,9 @@ from django.urls import path,include
 from rest_framework_simplejwt.views import ( TokenObtainPairView, TokenRefreshView)
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Employees.urls')),
-    path('timesheet/', include('timesheet.urls')),
-    path('auth/', include('rest_framework.urls')),
-    path('token/access/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include('Employees.urls')),
+    path('api/timesheet/', include('timesheet.urls')),
+    # path('auth/', include('rest_framework.urls')),
+    path('api/token/access/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
